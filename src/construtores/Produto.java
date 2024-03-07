05ed2c4a5bf7341dc67c2926e79fc241dbd0064e
silva3265 <silva3265@gmail.com>
2024-03-07 14:37:12 -0300

    Sobrecarga de construtores

--- src/construtores/Produto.java
+++ src/construtores/Produto.java
@@ -7,10 +7,19 @@ public class Produto {
 	String nome;
 	int quantidadeEstoque;
 
+	Produto() {
+		this.nome = "Sem nome";
+		this.quantidadeEstoque = QUANTIDADE_ESTOQUE_INICIAL;
+	}
+
 	Produto(String nome) {
 		this.nome = nome;
 		this.quantidadeEstoque = QUANTIDADE_ESTOQUE_INICIAL;
 	}
-	
+
+	Produto(String nome, int estoqueInicial) {
+		this.nome = nome;
+		this.quantidadeEstoque = estoqueInicial;
+	}
 
 }
