e772c590dc54cad2109244eda3b8a0daa0fa143c
silva3265 <silva3265@gmail.com>
2024-03-07 15:53:04 -0300

    Modificador final em variáveis de instância

--- src/construtores/Principal.java
+++ src/construtores/Principal.java
@@ -7,12 +7,15 @@ public class Principal {
 		Produto produto2 = new Produto("Arroz", 35);
 		Produto produto3 = new Produto();
 
+		System.out.println(produto1.codigo);
 		System.out.println(produto1.nome);
 		System.out.println(produto1.quantidadeEstoque);
 
+		System.out.println(produto2.codigo);
 		System.out.println(produto2.nome);
 		System.out.println(produto2.quantidadeEstoque);
 
+		System.out.println(produto3.codigo);
 		System.out.println(produto3.nome);
 		System.out.println(produto3.quantidadeEstoque);
 	}
