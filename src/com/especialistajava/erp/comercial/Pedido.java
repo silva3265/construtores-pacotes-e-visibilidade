57e5e11afee178e247972e365ecea6378d14554a
silva3265 <silva3265@gmail.com>
2024-03-07 16:45:31 -0300

    Importando classes de pacotes

--- src/com/especialistajava/erp/comercial/Pedido.java
+++ src/com/especialistajava/erp/comercial/Pedido.java
@@ -1,5 +1,12 @@
 package com.especialistajava.erp.comercial;
 
+import java.util.ArrayList;
+
+import com.especialistajava.erp.estoque.Produto;
+
 public class Pedido {
+	
+	ArrayList<Produto> produtos;
+    Cliente cliente;
 
 }
---
+++ src/com/especialistajava/erp/estoque/Principal.java
@@ -0,0 +1,15 @@
+package com.especialistajava.erp.estoque;
+
+import com.especialistajava.erp.comercial.Pedido;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Pedido pedido = new Pedido();
+		Produto produto = new Produto();
+
+		java.lang.System.out.println("Olá");
+		java.lang.String nome = "Thiago";
+	}
+
+}
