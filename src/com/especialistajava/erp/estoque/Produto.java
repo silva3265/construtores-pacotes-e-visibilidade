6ee05a47c4d0527cfed40d974df8d8fe77787fdc
silva3265 <silva3265@gmail.com>
2024-03-08 15:27:03 -0300

    Modificador de acesso private

--- src/com/especialistajava/erp/estoque/Produto.java
+++ src/com/especialistajava/erp/estoque/Produto.java
@@ -7,9 +7,9 @@ public class Produto {
 
 	static final int QUANTIDADE_ESTOQUE_INICIAL = 100;
 
-	final String codigo;
-	String nome;
-	int quantidadeEstoque;
+	private final String codigo;
+	private String nome;
+	private int quantidadeEstoque;
 
 	public Produto() {
 		this("Sem nome");
